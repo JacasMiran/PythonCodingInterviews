2d3cec4bf54282a5c2d39c0cf1e9cd8407c7543f
JacasMiran <rodolforafael15@gmail.com>
2022-06-27 18:49:11 -0300

    TExt

---
+++ Bulbds.py
@@ -0,0 +1,10 @@
+# Given N bulds, eiter on (1) or off (0).
+# Turning on ith buld causes all remaining bulbs on
+# to the right to flip
+
+# Find the min number of swithches to turn all the
+# bulbs on.
+
+# Constraints:
+# 1<=N <=1e5
+# A[i]= {0,1} 
